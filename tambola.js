22a05dee663e4bc10a1886f001bfd6b1828fa24d
bhavanig625 <125764357+bhavanig625@users.noreply.github.com>
2023-02-27 16:02:28 +0530

    UX fixes

--- tambola.js
+++ tambola.js
@@ -1,11 +1,23 @@
 var max = 90;
 var min = 1;
-var randList = RandomList(min, max);
+var randList;
 var current = 0;
 var intervalID;
 var timeInterval;
-var originalDOM;
-var isMute;
+var isMute = 0;
+var doAnnounce = 1;
+var playButton = document.getElementById("play");
+var pauseButton = document.getElementById("pause");
+var resetButton = document.getElementById("reset");
+var muteButton = document.getElementById("mute");
+var unmuteButton = document.getElementById("unmute");
+var generateButton = document.getElementById("manual-generate");
+var autoGenerateButton = document.getElementById("auto-generate");
+var manualResetButton = document.getElementById("manual-reset");
+var generateSection = document.getElementById("generate");
+var pace;
+var speedupButton = document.getElementById("speedup");
+var slowdownButton = document.getElementById("slowdown");
 
 window.onload = displayBoard;
 function displayBoard() {
@@ -18,8 +30,49 @@ function displayBoard() {
     numElement.appendChild(document.createTextNode(currentVal));
     containerElement.appendChild(numElement);
   }
-  originalDOM = document.body.innerHTML;
+  if (sessionStorage.getItem("lastIndex") > 0) {
+    document.getElementById("resume-restart").style.display = "block";
+    generateSection.style.display = "None";
+  }
 }
+
+function resume() {
+  sessionGameGenerator();
+  generateSection.style.display = "none";
+  document.getElementById("play-btns").style.display = "flex";
+  document.getElementById("resume-restart").style.display = "none";
+}
+
+function sessionGameGenerator() {
+  var previousIndex = Number(sessionStorage.getItem("lastIndex"));
+  timeInterval = Number(sessionStorage.getItem("lastTimeInterval"));
+  randList = sessionStorage.getItem("randomList").split(",");
+  doAnnounce = 0;
+  for (var i = 0; i <= previousIndex - 1; i++) {
+    getNext();
+  }
+  doAnnounce = 1;
+  intervalID = setInterval(getNext, timeInterval);
+  toggle();
+}
+
+function restart() {
+  generateSection.style.display = "block";
+  document.getElementById("resume-restart").style.display = "none";
+  sessionStorage.lastIndex = 0;
+  sessionStorage.removeItem("randomList");
+  sessionStorage.removeItem("lastTimeInterval");
+}
+
+window.onbeforeunload = () => {
+  pause();
+  sessionStorage.setItem("lastIndex", current);
+  sessionStorage.setItem("randomList", randList);
+  sessionStorage.setItem("lastTimeInterval", timeInterval);
+  document.getElementById("resume-restart").style.display = "block";
+  document.getElementById("play-btns").style.display = "none";
+  return false;
+};
 
 function announceText(text) {
   if (isMute != 1) {
@@ -37,61 +90,143 @@ function announceText(text) {
 function showNum(num) {
   var numElement = document.getElementById("show-num");
   numElement.innerText = num;
-  numElement.style.display = "flex";
+  numElement.style.visibility = "visible";
 }
 
 function generateNum() {
+  autoGenerateButton.style.display = "none";
+  generateButton.innerText = "Next";
+  document.getElementById("manual-reset").style.display = "flex";
+  if (current == 0) {
+    randList = RandomList(min, max);
+  }
   getNext();
 }
 
 function autoGenerateNum() {
-  var paceElements = document.getElementById("pace-section");
-  paceElements.style.display = "block";
-  //intervalID = setInterval(getNext, 100);
+  randList = RandomList(min, max);
+  document.getElementById("pace-section").style.display = "flex";
+  generateSection.style.display = "none";
 }
 
 function slowGenerator() {
-  timeInterval = 6000;
+  pace = 1;
+  setTimeInterval(pace);
   intervalID = setInterval(getNext, timeInterval);
+  //slowdownButton.style.display = "none";
   toggle();
 }
 function mediumGenerator() {
-  timeInterval = 3500;
+  pace = 2;
+  setTimeInterval(pace);
   intervalID = setInterval(getNext, timeInterval);
   toggle();
 }
 function fastGenerator() {
-  timeInterval = 2000;
+  pace = 3;
+  setTimeInterval(pace);
   intervalID = setInterval(getNext, timeInterval);
+  //speedupButton.style.display = "none";
   toggle();
 }
 
+function setTimeInterval(speedPace) {
+  switch (speedPace) {
+    case 1:
+      timeInterval = 6000;
+      slowdownButton.style.visibility = "hidden";
+      speedupButton.style.visibility = "visible";
+      break;
+    case 2:
+      timeInterval = 3500;
+      slowdownButton.style.visibility = "visible";
+      speedupButton.style.visibility = "visible";
+      break;
+    case 3:
+      timeInterval = 2000;
+      slowdownButton.style.visibility = "visible";
+      speedupButton.style.visibility = "hidden";
+      break;
+  }
+}
+
+function speedup() {
+  if (pace < 3) {
+    pace += 1;
+    clearInterval(intervalID);
+    setTimeInterval(pace);
+    intervalID = setInterval(getNext, timeInterval);
+    console.log(timeInterval);
+  }
+}
+
+function slowdown() {
+  if (pace > 0) {
+    pace -= 1;
+    clearInterval(intervalID);
+    setTimeInterval(pace);
+    intervalID = setInterval(getNext, timeInterval);
+    console.log(timeInterval);
+  }
+}
+
 function toggle() {
-  var paceElements = document.getElementById("pace-section");
-  paceElements.style.display = "None";
-  var playPauseElements = document.getElementById("play-pause");
-  playPauseElements.style.display = "block";
+  generateButton.disabled = true;
+  document.getElementById("pace-section").style.display = "none";
+  document.getElementById("play-btns").style.display = "flex";
 }
 
 function play() {
   intervalID = setInterval(getNext, timeInterval);
+  pauseButton.style.display = "inline-block";
+  playButton.style.display = "none";
 }
 
 function pause() {
   clearInterval(intervalID);
+  pauseButton.style.display = "none";
+  playButton.style.display = "inline-block";
+}
+
+function manualReset() {
+  if (confirm("Are you sure, you want to reset game?") == true) {
+    document.getElementById("show-num").style.visibility = "hidden";
+    current = 0;
+    randList;
+  }
 }
 
 function reset() {
   clearInterval(intervalID);
-  document.body.innerHTML = originalDOM;
+  if (confirm("Are you sure, you want to reset game?") == true) {
+    //clearInterval(intervalID);
+    sessionStorage.clear();
+    document.getElementById("tambola-board").innerHTML = "";
+    document.getElementById("play-btns").style.display = "none";
+    document.getElementById("show-num").style.visibility = "hidden";
+    generateSection.style.display = "flex";
+    document.getElementById("resume-restart").style.display = "none";
+    generateButton.disabled = false;
+    pauseButton.style.display = "inline-block";
+    playButton.style.display = "none";
+    displayBoard();
+    generateButton.innerText = "Generate";
+    autoGenerateButton.style.display = "flex";
+    manualResetButton.style.display = "none";
+    current = 0;
+  }
 }
 
 function mute() {
   isMute = 1;
+  muteButton.style.display = "none";
+  unmuteButton.style.display = "inline-block";
 }
 
 function unmute() {
   isMute = 0;
+  muteButton.style.display = "inline-block";
+  unmuteButton.style.display = "none";
 }
 
 function getNext() {
@@ -100,12 +235,18 @@ function getNext() {
     var num = randList[current];
     var element = document.getElementById("num-" + num);
     element.classList.add("current");
-    announceText(element.textContent);
-    showNum(element.textContent);
+    if (doAnnounce == 1) {
+      announceText(element.textContent);
+      showNum(element.textContent);
+    }
     if (current > 0) {
       var previousNum = randList[current - 1];
       var prevElement = document.getElementById("num-" + previousNum);
-      prevElement.classList.replace("current", "generated");
+      if (prevElement.classList.contains("current")) {
+        prevElement.classList.replace("current", "generated");
+      } else {
+        prevElement.classList.add("generated");
+      }
     }
     current += 1;
   }
